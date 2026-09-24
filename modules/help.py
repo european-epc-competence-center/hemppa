284bc940ff735e236fcf7783deedaf68caf66c07
Dennis Borrmann <debbo@hotmail.de>
2026-09-24 12:01:26 +0200

    reset

--- modules/help.py
+++ modules/help.py
@@ -6,7 +6,6 @@ class MatrixModule(BotModule):
     def __init__(self, name):
         super().__init__(name)
         self.msg_users = False
-        self.info = "\nMore information at https://github.com/vranki/hemppa"
 
     def get_settings(self):
         data = super().get_settings()
